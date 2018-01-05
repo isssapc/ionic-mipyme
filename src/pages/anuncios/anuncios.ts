f6a67529e04acc912c01bb40a1da1c7962831786
Ramiro Arechar <ramiro@arechar>
2018-01-05 15:48:25 -0600

    agregamos el badge con el número de productos que contiene el pedido

--- src/pages/anuncios/anuncios.ts
+++ src/pages/anuncios/anuncios.ts
@@ -13,35 +13,7 @@ export class AnunciosPage {
 
   pedido: any[] = [];
 
-  anuncios: any[];/*  [
-    {
-      titulo: "Pants Deportivos",
-      descripcion: "Alta resistencia e ideal para deportes",
-      fecha_publicacion: "2018-12-26",
-      img_src: "assets/imgs/pants.jpg",
-      precio: "$3,000.00",
-      categoria: "Promociones",
-      avatar_src: "assets/imgs/logo.png"
-    },
-    {
-      titulo: "Short Deportivo",
-      descripcion: "ideal para los que practican running",
-      fecha_publicacion: "2018-12-26",
-      img_src: "assets/imgs/short.jpg",
-      precio: "$1,000.00",
-      categoria: "Promociones",
-      avatar_src: "assets/imgs/logo.png"
-    },
-    {
-      titulo: "Conjunto Deportivo para Dama",
-      descripcion: "Bonito conjunto deportivo, unitalla.",
-      fecha_publicacion: "2018-12-26",
-      img_src: "assets/imgs/conjunto.jpg",
-      precio: "$2,000.00",
-      categoria: "Promociones",
-      avatar_src: "assets/imgs/logo.png"
-    }
-  ]; */
+  anuncios: any[];
 
   constructor(
     public navCtrl: NavController,
@@ -70,11 +42,14 @@ export class AnunciosPage {
   agregarAlPedido(producto) {
 
     console.log("agregado al pedido", producto);
-    
 
-    producto.cantidad = 1;
+    let i = this.pedido.indexOf(producto);
+    if (i == -1) {
 
-    this.pedido.push(producto);
+      producto.cantidad = 1;
+      this.pedido.push(producto);
+      
+    }
 
   }
 
